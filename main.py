ca09fe137b4ec5c11299887731af68a9807eeeb7
Neeschal1 <neeschalpok04@gmail.com>
2025-08-01 18:45:00 +0545

    numpy basics

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr_one = np.array([127, 127, 127])
+arr_two = np.array([[1, 2, 3], [4, 5, 6]], dtype=np.int8)
+arr_three = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+print(arr_one, "\n\n", arr_two, "\n\n", arr_three, arr_one.dtype)
+
