695c6246df9b1afeda94b42539055d547b4ba4bc
Neeschal1 <neeschalpok04@gmail.com>
2025-09-17 19:58:11 +0545

    about the various array functions such as sum, average, minimum value, maximum value and so on...

---
+++ array_functions.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+matrix = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+print(matrix.sum()) # Calculates the sum of all the elements inside the array.
+print(matrix.mean()) # Calculates the average value of elements inside the array.
+print(matrix.min()) # Finds the minimum value among all the array elements.
+print(matrix.max()) # Finds the maximum value among all the array elements.
+print(matrix.std()) # Finds the standard deviation of the array elements.
+print(matrix.var()) # Finds the variance of the array elements.
