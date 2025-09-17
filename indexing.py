a8caf328adfa15b9478c83f36a86fdef9a0b24b2
Neeschal1 <neeschalpok04@gmail.com>
2025-09-17 20:02:52 +0545

    indexing the elements inside the array

---
+++ indexing.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+matrix = np.array([10, 20, 30, 40, 50])
+
+# Indexing in python begins from 0.
+# The last value can be accessed by -1 index. And the elements from last can be accessed by -1, -2, -3 respectively. 
+print(matrix[0]) 
+print(matrix[3])
+print(matrix[-1])
